71a4e2f509e86f35ce4853fa4023f49657cafb0a
seanwayland <seanwayland@gmail.com>
2021-01-23 19:23:37 -0500

    Create buystock.py

---
+++ buystock.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if prices==[]:
+                return 0
+        buy = prices[0]
+        mx_profit = 0
+
+        for i in range(1,len(prices)):
+            profit = prices[i]-buy
+
+            if profit>mx_profit:
+                mx_profit = profit
+
+            if buy>prices[i]:
+                buy = prices[i]
+
+        return mx_profit
